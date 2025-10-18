c617bbbe10144606161bb7c9c5bd4080cb20845c
Alena Drobner <alena.drobner@gmail.com>
2025-10-18 11:58:29 +0200

    Improve card HTML serialization and fix UTF-8 encoding for apostrophes

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,36 +2,40 @@ import json
 
 def load_data(file_path):
     """Loads a JSON file"""
-    with open(file_path, "r") as handle:
+    with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
 # Daten laden
 animal_data = load_data("animals_data.json")
 
-
 output = ""
 
 for animal in animal_data:
-    output += '<li class="cards__item">\n'
 
-    if "name" in animal:
-        output += f"Name: {animal['name']}<br/>\n"
+    name = animal.get("name", "")
+    name = name.replace("’", "'").replace("‘", "'")
 
-    if "characteristics" in animal and "diet" in animal["characteristics"]:
-        output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
-
+    characteristics = animal.get("characteristics", {})
+    diet = characteristics.get("diet", "")
+    type_ = characteristics.get("type", "")
+    location = ""
     if "locations" in animal and animal["locations"]:
-        output += f"Location: {animal['locations'][0]}<br/>\n"
+        location = animal["locations"][0]
 
-    if "characteristics" in animal and "type" in animal["characteristics"]:
-        output += f"Type: {animal['characteristics']['type']}<br/>\n"
 
-    output += "</li>\n"
-
+    output += (
+        '<li class="cards__item">\n'
+        f'  <div class="card__title">{name}</div>\n'
+        '  <p class="card__text">\n'
+        f'      <strong>Diet:</strong> {diet}<br/>\n'
+        f'      <strong>Location:</strong> {location}<br/>\n'
+        f'      <strong>Type:</strong> {type_}<br/>\n'
+        '  </p>\n'
+        '</li>\n'
+    )
 
 print(output)
 print("li-count:", output.count('<li class="cards__item">'))
-
 
 with open("animals_template.html", "r", encoding="utf-8") as f:
     template_html = f.read()
@@ -43,4 +47,3 @@ with open("animals.html", "w", encoding="utf-8") as f:
 
 print("li-count:", output.count('<li class="cards__item">'))
 print("wrote animals.html")
-
